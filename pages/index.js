b0636a948008f4985c63b52ed2efe43369531d17
ilfrusciante <rob72rom@gmail.com>
2025-04-17 01:54:20 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import Navbar from '../components/Navbar'; // <--- Import in cima
+
 export default function Home() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Benvenuto su Connectiamo</h1>
-      <p>La piattaforma per connettere segnalatori e professionisti.</p>
+    <div>
+      <Navbar />  {/* <--- Qui viene mostrata la barra */}
+      <div style={{ padding: 20 }}>
+        <h1>Benvenuto su Connectiamo</h1>
+        <p>La piattaforma per connettere segnalatori e professionisti.</p>
+      </div>
     </div>
   );
 }
-

3deaa8b5b76b3788689de2113611040a132b91c5
ilfrusciante <rob72rom@gmail.com>
2025-04-16 22:17:48 +0200

    Update index.js
    Aggiunge pages/index.js per la home page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Benvenuto su Connectiamo</h1>
+      <p>La piattaforma per connettere segnalatori e professionisti.</p>
+    </div>
+  );
+}
 

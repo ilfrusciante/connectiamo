dfe053b1db7c2ade50f63aedbf9379ff044c0685
ilfrusciante <rob72rom@gmail.com>
2025-04-16 21:46:05 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Benvenuto su Connectiamo</h1>
+      <p>La piattaforma per connettere segnalatori e professionisti.</p>
+    </div>
+  );
+}
